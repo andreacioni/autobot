4cb6fa657004fdddf589782d3d4c41efc6d645df
andreacioni <supercionci@hotmail.it>
2018-07-29 23:03:24 +0200

    Releasing v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="telegram-autobot",
-    version="0.0.3",
+    version="0.0.4",
     author="Andrea Cioni",
     author_email="cioni95@rocketmail.com",
     description="Build an interactive Telegram bot with some JSON files",
