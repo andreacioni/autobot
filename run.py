642bfea9b8714c983ac5149b8087b5c4e903a397
andreacioni <supercionci@hotmail.it>
2018-08-10 00:17:09 +0200

    mantain compatibility

--- run.py
+++ run.py
@@ -2,6 +2,34 @@
 
 """ Main module that start """
 
+import logging
+import argparse
+
+from autobot.version import name, version
+
+""" Entry point of Autobot application"""
+def main():
+    #Parsing arguments
+    parser = argparse.ArgumentParser('{} - v.{}'.format(name, version))
+    parser.add_argument('settings_file', \
+                        help='file containing the main setting of autobot')
+    parser.add_argument('config_dir', \
+                        help='file containing the configuration for autobot istance')
+    parser.add_argument('-l', '--log_level', \
+                        metavar='log_level', \
+                        default='WARN', \
+                        choices=['DEBUG', 'INFO', 'WARN', 'ERROR'], \
+                        help='file containing the configuration for autobot istance')
+
+    args = parser.parse_args()
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', \
+                    level=getattr(logging, args.log_level))
+
+    from autobot import AutoBot
+
+    AutoBot(args.settings_file, args.config_dir).startup()
+
 if __name__ == '__main__':
-    import autobot
-    autobot.main()
+    main()
