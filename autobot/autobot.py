8e8af6833aaf087af05dfcef6090d97ee787cb1c
Andrea Cioni <supercionci@hotmail.it>
2025-01-06 16:02:06 +0100

    Migration to python-telegram-bot 21.6

--- autobot/autobot.py
+++ autobot/autobot.py
@@ -8,7 +8,7 @@ import argparse
 
 from sys import version_info as pyversion
 
-from telegram.ext import Updater, ConversationHandler
+from telegram.ext import Updater, ConversationHandler, ApplicationBuilder
 
 from .version import name, version
 from .configuration import AutoBotConfig, AutoBotSettings
@@ -18,9 +18,9 @@ from .plugins import PluginsLoader
 # Loading dynamic plugin modules before starting
 PluginsLoader.load()
 
-class AutoBot(object):
 
-    """ This is the main class of AutoBot, almost all work is done here """
+class AutoBot(object):
+    """This is the main class of AutoBot, almost all work is done here"""
 
     def __init__(self, settings_file, configuration_dir):
         self._logger = logging.getLogger(__name__)
@@ -31,17 +31,19 @@ class AutoBot(object):
         pass
 
     def _parse_config(self):
-        """ Transforming entries in appropriate handler """
+        """Transforming entries in appropriate handler"""
         entries = self._config.get()
 
-        self._logger.info('Parsing entries content')
+        self._logger.info("Parsing entries content")
         handlers = {}
         entry_points = []
         fallbacks = []
 
         for entry in entries:
             # Build an appropriate handler based on given configuration
-            hdl = CallbackHandlerFactory.build_callback(self._settings, self._config, entry)
+            hdl = CallbackHandlerFactory.build_callback(
+                self._settings, self._config, entry
+            )
 
             # Add built handler to the appropriate group (state). Remember that every entry
             # could belong to multiple groups
@@ -60,7 +62,7 @@ class AutoBot(object):
 
     def _register_handlers(self, dispatcher):
 
-        self._logger.info('Registering handlers')
+        self._logger.info("Registering handlers")
 
         entry_points, fallbacks_list, handlers_dict = self._parse_config()
 
@@ -73,41 +75,43 @@ class AutoBot(object):
 
         # At least one entry point has to be defined in configuration
         if not entry_points:
-            raise RuntimeError("At least one entry point has to be defined in configuration")
+            raise RuntimeError(
+                "At least one entry point has to be defined in configuration"
+            )
 
-        dispatcher.add_handler(ConversationHandler(
-            entry_points,
-            handlers_dict,
-            fallbacks_list,
-            allow_reentry=reentry
-        ))
-
+        dispatcher.add_handler(
+            ConversationHandler(
+                entry_points, handlers_dict, fallbacks_list, allow_reentry=reentry
+            )
+        )
 
     def startup(self):
-        """ Starting point of AutoBot program"""
+        """Starting point of AutoBot program"""
 
-        self._logger.info('Starting AutoBot - v.0.0.1 (on Python %i.%i.%i)', pyversion.major, pyversion.minor, pyversion.micro)
+        self._logger.info(
+            "Starting AutoBot - v.0.0.1 (on Python %i.%i.%i)",
+            pyversion.major,
+            pyversion.minor,
+            pyversion.micro,
+        )
 
-        if(pyversion.major < 3):
-            self._logger.warn('Detected Python version 2.x, consider to switch to newer version')
+        if pyversion.major < 3:
+            self._logger.warn(
+                "Detected Python version 2.x, consider to switch to newer version"
+            )
 
         # Create the EventHandler and pass it your bot's token.
-        self._logger.info('Creating updater...')
-        updater = Updater(self._settings[AutoBotSettings.API_KEY],\
-                          workers=self._settings[AutoBotSettings.WORKERS])
-
-        # Get the dispatcher to register handlers
-        dispatcher = updater.dispatcher
+        self._logger.info("Creating updater...")
+        app = (
+            ApplicationBuilder().token(self._settings[AutoBotSettings.API_KEY]).build()
+        )
 
         # Register handlers to the dispatcher object
-        self._register_handlers(dispatcher)
+        self._register_handlers(app)
 
-        self._logger.info('Everything done! Starting polling phase...')
+        self._logger.info("Everything done! Starting polling phase...")
 
-        # Start the Bot
-        updater.start_polling()
-
-        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # Start the Bot and run until you press Ctrl-C or the process receives SIGINT,
         # SIGTERM or SIGABRT. This should be used most of the time, since
         # start_polling() is non-blocking and will stop the bot gracefully.
-        updater.idle()
+        app.run_polling()
